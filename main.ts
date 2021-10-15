2e12b04a1d71a157f836f9b1e3459c1dd3801319
Joshua McLean <jayshoo@gmail.com>
2021-10-16 00:44:06 +1100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-async function hltbSearch(search: string): unknown {
+interface SearchResult {
+  id: string
+  label: string
+}
+
+async function hltbSearch(search: string): SearchResult | null {
   let body = new URLSearchParams(`t=games&sorthead=popular&sortd=0&plat=&length_type=main&length_min=&length_max=&v=&f=&g=&detail=&randomize=0`)
   body.set('queryString', search)
   console.log('searching', search, body)
@@ -10,13 +15,23 @@ async function hltbSearch(search: string): unknown {
     },
     body
   })
-  return result
+  
+  let text = await result.text()
+  
+  let re = /<ul>.*<li.*<a aria-label="(.+)".*href="game\?id=(\d+)"/si
+  let matches = re.exec(text)
+  if (!matches) return null
+  
+  let [_, label, id] = matches
+  return { id, label }
 }
 
 async function hltb(search: string): Promise<Response> {
-  let result = await hltbSearch(search)
+  let searchResult = await hltbSearch(search)
+  if (!searchResult)
+    return new Response(`Couldn't find ${search}. Soz`)
   
-  return new Response(result.body)
+  return new Response(`Found ${searchResult.label} with id=${searchResult.id}`)
 }
 
 addEventListener('fetch', event => {
