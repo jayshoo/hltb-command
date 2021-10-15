5ff2869ce52ecaf01484ce91633d9738fc1e08eb
Joshua McLean <jayshoo@gmail.com>
2021-10-16 00:05:49 +1100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { HowLongToBeatService, HowLongToBeatEntry } from 'howlongtobeat'
+import { HowLongToBeatService, HowLongToBeatEntry } from 'https://cdn.skypack.dev/howlongtobeat'
 
 let hltbService = new HowLongToBeatService()
 
